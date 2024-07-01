9e297c7dcbd331788467330bc69c65ff28adc757
shoukathmd <shoukathmd@gmail.com>
2024-07-01 18:14:44 +0530

    Removed the scale

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -3,24 +3,48 @@
 import React, { useState } from "react";
 import { generateChatResponse } from "../utils/actions";
 import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Chat = () => {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
-  const { mutate } = useMutation({
-    mutationFn: (text) => generateChatResponse(text),
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      const response = await generateChatResponse(messages);
+
+      if (!response) {
+        toast.error("Something went wrong...");
+        return;
+      }
+      setMessages((prev) => [...prev, response.message]);
+      const newTokens = await subtractTokens(userId, response.tokens);
+      toast.success(`${newTokens} tokens remaining...`);
+    },
   });
   const handleSubmit = (e) => {
     e.preventDefault();
-    mutate(text);
-    console.log(text);
+    const query = { role: "user", content: text };
+    setMessages((prev) => [...prev, query]);
+    setText("");
+    mutate();
   };
   return (
-    <div className="min-h-[calc(100vh-8rem)] grid grid-rows-[1fr,auto] min-h-screen">
+    <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div className="text-5xl">
-        <h1 className="text-3xl mb-4">Messages</h1>
-        {/* Example messages */}
-        <p className="mb-2">Messages</p>
+        {messages.map(({ role, content }, index) => {
+          const avatar = role == "user" ? "👤" : "🤖";
+          const bcg = role === "user" ? "bg-base-200" : "bg-base-100";
+          return (
+            <div
+              key={index}
+              className={`${bcg} flex py-6 px-8 text-xl leading-loose border-b border-base-300`}
+            >
+              <span className="mr-4">{avatar}</span>
+              <p className="max-w-3xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending ? <span className="loading"></span> : null}
       </div>
       <form
         onSubmit={handleSubmit}
@@ -30,11 +54,19 @@ const Chat = () => {
         <div className="join w-full">
           <input
             type="text"
-            onChange={(e) => setText(e.target.value)}
+            placeholder="Message GeniusGPT"
             className="input input-bordered join-item w-full"
-            placeholder="Type your message..."
+            value={text}
+            required
+            onChange={(e) => setText(e.target.value)}
           />
-          <button className="btn btn-primary join-item">Ask question</button>
+          <button
+            className="btn btn-primary join-item"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending ? "Please Wait..." : "Ask Question"}
+          </button>
         </div>
       </form>
     </div>
--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,44 @@
 "use server";
+import OpenAI from "openai";
 
-export const generateChatResponse = (chatMessages) => {
-  console.log(chatMessages);
-  return chatMessages;
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const generateChatResponse = async (chatMessages) => {
+  try {
+    console.log(chatMessages);
+    // Validate chatMessages format
+    // if (!Array.isArray(chatMessages)) {
+    //   throw new Error("chatMessages should be an array");
+    // }
+
+    chatMessages.forEach((message, index) => {
+      if (
+        typeof message !== "object" ||
+        !message.hasOwnProperty("role") ||
+        !message.hasOwnProperty("content")
+      ) {
+        throw new Error(`Invalid message format at index ${index}`);
+      }
+    });
+
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "you are a helpful assistant" },
+        ...chatMessages,
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      max_tokens: 100,
+    });
+
+    return {
+      message: response.choices[0].message,
+      tokens: response.usage.total_tokens,
+    };
+  } catch (error) {
+    console.log("Error:", error.message);
+    return null;
+  }
 };
